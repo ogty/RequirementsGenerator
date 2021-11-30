4e7a5cb329390b8734097fee288aaed9d8168968
ogty <mofuiti1803@gmail.com>
2021-12-01 02:42:00 +0900

    change

--- src/base.py
+++ src/base.py
@@ -7,13 +7,25 @@ import platform
 # Module Extractor
 class ModuleExtractor:
     def python(self, source: str, lang: str) -> list:
+        embedded: list = settings["languages"][lang][2]
         fixed_source = autopep8.fix_code(source)
-        result = self.common(lang, fixed_source)
-        return result
+        splited_source = fixed_source.split()
 
+        modules = [splited_source[i+1] for i, x in enumerate(splited_source) if x == "from" or x == "import"]
+        result = list(filter(lambda m: m.split(".")[0] if not m.startswith(".") else "", modules))
+        result = list(map(lambda x: "" if x in embedded else x, result))
+
+        return result
+    
     def julia(self, source: str, lang: str) -> list:
-        result = self.common(lang, source)
-        result = list(map(lambda d: d.replace(":", "").replace(";", ""), result))
+        embedded: list = settings["languages"][lang][2]
+        splited_source = source.split()
+
+        modules = [splited_source[i+1] for i, x in enumerate(splited_source) if x == "using" or x == "import"]
+        result = list(filter(lambda m: m.split(".")[0] if not m.startswith(".") else "", modules))
+        result = list(map(lambda m: m.replace(":", "").replace(";", ""), result))
+        result = list(map(lambda x: "" if x in embedded else x, result))
+
         return result
 
     def go(self, source: str, lang: str) -> list:
@@ -39,29 +51,6 @@ class ModuleExtractor:
 
         return result
 
-    def common(self, lang: str, source: str) -> list:
-        result = list()
-        prefix = settings["languages"][lang][1]
-        embedded = settings["languages"][lang][2]
-
-        if len(prefix) >= 2:
-            process_list = list(map(lambda x: f"line.startswith('{x}')", prefix))
-            process = " or ".join(process_list)
-        elif len(prefix) == 1:
-            process = f"line.startswith('{prefix[0]}')"
-
-        source_list = source.split("\n")
-        for line in source_list:
-            if eval(process):
-                module = line.split(" ")[1]
-                if not module.startswith("."):
-                    module = module.split(".")[0]
-                    result.append(module)
-
-        # Remove embedded libraries
-        result = list(map(lambda x: "" if x in embedded else x, result))
-        return result
-
 class RequirementsGenerator:
     # initialize valiables and run function
     def __init__(self, path: str, lang: str) -> None:
@@ -69,6 +58,8 @@ class RequirementsGenerator:
         self.lang = lang
         self.all_file = list()
         self.all_dir: list = [path]
+        os_name = platform.system()
+        self.split_word = settings["os"][os_name][1]
 
         self.get_dirs(self.path)
         self.get_files()
@@ -80,16 +71,17 @@ class RequirementsGenerator:
         files_dir = [f for f in base if os.path.isdir(os.path.join(path, f))]
 
         for dir in files_dir:
-            self.all_dir.append(f"{path}/{dir}")
-            self.get_dirs(f"{path}/{dir}")
+            self.all_dir.append(f"{path}{self.split_word}{dir}")
+            self.get_dirs(f"{path}{self.split_word}{dir}")
 
     # Retrieves a specific file in the retrieved directory.
     def get_files(self) -> None:
         for dir in self.all_dir:
             base = os.listdir(dir)
-            files = [f for f in base if os.path.isfile(os.path.join(dir, f))]                      # get only files
-            files = list(filter(lambda f: f.endswith(settings["languages"][self.lang][0]), files)) # extension
-            files = list(map(lambda f: f"{dir}/{f}", files))                                       # create absolute path
+            files = [f for f in base if os.path.isfile(os.path.join(dir, f))]
+            files = list(filter(lambda f: f.endswith(settings["languages"][self.lang][0]), files))
+            files = list(map(lambda f: f"{dir}{self.split_word}{f}", files))
+
             self.all_file += files
 
     # Main process(generate)
@@ -121,7 +113,7 @@ def generate_tree():
     # Get all directory information directly under the default path written in settings.json
     os_name = platform.system()
     user_name = os.getlogin()
-    path = settings["os"][os_name].replace("<user_name>", user_name)
+    path = settings["os"][os_name][0].replace("<user_name>", user_name)
     
     # Store the retrieved information in a dict
     main_data = {"data": list()}
