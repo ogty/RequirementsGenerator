0265cb1f59dff4c2669a03102c3f31cfa51a1189
ogty <mofuiti1803@gmail.com>
2021-12-01 09:43:49 +0900

    fix

--- routes.py
+++ routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import jsonify, request, render_template
 import os
 import json
+import platform
 
 from src.base import RequirementsGenerator, generate_tree
 
@@ -33,20 +34,17 @@ def detail():
     dirs = list(set(selected_dirs.split(",")))
 
     detail_data = RequirementsGenerator().detail(dirs=dirs)
-    data = {"detail_data": dict()}
-    data["detail_data"] = detail_data
     return jsonify(values=json.dumps(detail_data))
 
 # base
 @bp.route("/")
 def base():
-    if not os.path.exists(f"{os.getcwd()}\\static\\tree.json"):
+    if not os.path.exists(f"{os.getcwd()}{split_word}static{split_word}tree.json"):
         generate_tree()
 
     lang_data = {lang.capitalize(): lang for lang in settings["languages"]}
     return render_template("main.html", data=lang_data)
 
-
-# Global variables are currently more efficient. maybe...
-data = open(f"{os.getcwd()}\\src\\settings.json", "r")
+split_word = "\\" if platform.system() == "Windows" else "/"
+data = open(f"{os.getcwd()}{split_word}src{split_word}settings.json", "r")
 settings = json.load(data)
