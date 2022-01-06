a62011848b5a433a98296c4d4bda374f27269b3e
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-01-07 06:13:50 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,7 @@
 import json
 import os
-import platform
 
 
-SPLIT_WORD = "\\" if platform.system() == "Windows" else "/"
-data = open(f"{os.getcwd()}{SPLIT_WORD}static{SPLIT_WORD}settings.json", "r")
-SETTINGS = json.load(data)
-TREE_PATH = f"{os.getcwd()}{SPLIT_WORD}static{SPLIT_WORD}tree.json"
+data = open(f"{os.getcwd()}/static/config.json", "r")
+CONFIG = json.load(data)
+TREE_PATH = f"{os.getcwd()}/static/tree.json"
