276f7d10cb6723baf974b1079b48b2c02ac6f99b
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-01-07 13:42:10 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ import json
 import os
 
 
+DESKTOP_PATH = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
 TOP_DIR = os.path.dirname(__file__)
 STATIC_DIR = os.path.join(TOP_DIR, "static")
 TREE_PATH = os.path.join(STATIC_DIR, "tree.json")
