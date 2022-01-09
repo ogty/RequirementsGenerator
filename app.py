c51bb103a4028ece50d2671df6d6ca7e604a93d7
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-01-10 01:29:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from pyfladesk import init_gui
 
 from src import routes
 
+
 app = Flask(__name__)
 app.register_blueprint(routes.bp)
+
 
 if __name__ == "__main__":
     init_gui(app, window_title="Requirements.txt Generator")
