9ecd35b47837e94b395f67bbd8a7685765b6b4e6
ogty <mofuiti1803@gmail.com>
2022-01-10 02:00:37 +0900

    update

--- settings.py
+++ settings.py
@@ -1,8 +1,14 @@
 import json
 import os
+import platform
 
 
-DESKTOP_PATH = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
+os_name = platform.system()
+if os_name == "Darwin":
+    DESKTOP_PATH = os.path.join(os.path.join(os.environ["HOME"]), "Desktop")
+elif os_name == "Windows":
+    DESKTOP_PATH = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
+
 TOP_DIR = os.path.dirname(__file__)
 STATIC_DIR = os.path.join(TOP_DIR, "static")
 TREE_PATH = os.path.join(STATIC_DIR, "tree.json")
