3f76216007281272c2f4dd66062189ee0ab8d510
ogty <mofuiti1803@gmail.com>
2022-01-17 05:16:22 +0900

    update

--- test/main.py
+++ test/main.py
@@ -4,10 +4,7 @@ import subprocess
 def get_installed_libraries() -> list:
     stdout_result = subprocess.run(["pip3", "freeze"], capture_output=True)
     pip_freezed = stdout_result.stdout.decode("utf-8").split("\n")
-    
-    installed_libary_names = []
-    for installed_libary_and_version in pip_freezed:
-        installed_libary_names.append(installed_libary_and_version.split("==")[0])
+    installed_libary_names = [library_data.split("==")[0] for library_data in pip_freezed]
 
     return installed_libary_names
 
@@ -17,7 +14,3 @@ def is_install(module: str) -> bool:
         return True
     else:
         return False
-
-if __name__ == "__main__":
-    print(is_install("python-dotenv"))
-    print(is_install("dotenv"))
